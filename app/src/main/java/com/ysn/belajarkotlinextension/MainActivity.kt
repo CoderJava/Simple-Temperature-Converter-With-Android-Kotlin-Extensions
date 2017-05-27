8c93d1b36a41ebf9cae7fd9b7ce2c6d449a74520
CoderJava <kolonel.yudisetiawan@gmail.com>
2017-05-27 18:27:49 +0700

    Add listener onItemSelected in Spinner

--- app/src/main/java/com/ysn/belajarkotlinextension/MainActivity.kt
+++ app/src/main/java/com/ysn/belajarkotlinextension/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,8 +23,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
 
     private fun initComponent() {
-        // do something
         arrayUnitTemperature = arrayOf("C", "F", "K", "R", "Re")
+        val arrayUnitDescribeTemperature = arrayOf("Celcius", "Fahrenheit", "Kelvin", "Rankine", "Reaumur")
         val arrayAdapterUnitTemperature = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, arrayUnitTemperature)
         app_compat_spinner_unit_temperature_from_activity_main.adapter = arrayAdapterUnitTemperature
         app_compat_spinner_unit_temperature_to_activity_main.adapter = arrayAdapterUnitTemperature
@@ -43,64 +45,79 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button_keypad_ac_activity_main.setOnClickListener(this)
         button_keypad_dot_activity_main.setOnClickListener(this)
         button_keypad_plus_minus_activity_main.setOnClickListener(this)
+
+        app_compat_spinner_unit_temperature_from_activity_main.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // nothing to do in here
+            }
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, index: Int, id: Long) {
+                text_view_unit_temperature_from_activity_main.text = arrayUnitDescribeTemperature.get(index)
+                if (temperature != "0" && temperature != "0.0") {
+                    calculateTemperatureConversion()
+                }
+            }
+
+        }
+        app_compat_spinner_unit_temperature_to_activity_main.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                // nothing to do in here
+            }
+
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, index: Int, id: Long) {
+                text_view_unit_temperature_to_activity_main.text = arrayUnitDescribeTemperature.get(index)
+                if (temperature != "0" && temperature != "0.0") {
+                    calculateTemperatureConversion()
+                }
+            }
+
+        }
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
             R.id.button_keypad_0_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("0")
             }
             R.id.button_keypad_1_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("1")
             }
             R.id.button_keypad_2_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("2")
             }
             R.id.button_keypad_3_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("3")
             }
             R.id.button_keypad_4_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("4")
             }
             R.id.button_keypad_5_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("5")
             }
             R.id.button_keypad_6_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("6")
             }
             R.id.button_keypad_7_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("7")
             }
             R.id.button_keypad_8_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("8")
             }
             R.id.button_keypad_9_activity_main -> {
-                // do something
                 updateTemperatureValueFrom("9")
             }
             R.id.button_keypad_backspace_activity_main -> {
-                // do something
                 backspaceTemperatureValueFrom()
             }
             R.id.button_keypad_ac_activity_main -> {
-                // do something
                 clearTemperatureValueFrom()
             }
             R.id.button_keypad_dot_activity_main -> {
-                // do something
                 updateTemperatureValueFrom(".")
             }
             R.id.button_keypad_plus_minus_activity_main -> {
-                // do something
                 changePlusMinusValueFrom()
             }
             else -> {
@@ -160,7 +177,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         Log.d(TAG, "unitTemperatureTo: $unitTemperatureTo")
         val temperatureFrom = temperature.toDouble()
         var temperatureTo: Double = 0.0
-        Log.d(TAG, "temperatureFrom: $temperatureFrom && temperatureTo: $temperatureTo")
+        Log.d(TAG, "temperatureFrom: $temperatureFrom && temperatureTo: $temperatureTo && unitTemperatureFrom: $unitTemperatureFrom && unitTemperatureTo: $unitTemperatureTo")
 
         if (unitTemperatureFrom == "C") {
             when (unitTemperatureTo) {
@@ -182,7 +199,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     temperatureTo = temperatureFrom.times(0.8)
                 }
                 else -> {
-                    // nothing to do in here
+                    temperatureTo = temperatureFrom
                 }
             }
         } else if (unitTemperatureFrom == "F") {
@@ -204,7 +221,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     temperatureTo = (temperatureFrom.minus(32)).div(2.25)
                 }
                 else -> {
-                    // nothing to do in here
+                    temperatureTo = temperatureFrom
                 }
             }
         } else if (unitTemperatureFrom == "K") {
@@ -226,7 +243,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     temperatureTo = (temperatureFrom.minus(273.15)).div(0.8)
                 }
                 else -> {
-                    // nothing to do in here
+                    temperatureTo = temperatureFrom
                 }
             }
         } else if (unitTemperatureFrom == "R") {
@@ -249,6 +266,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 }
                 else -> {
                     // nothing to do in here
+                    temperatureTo = temperatureFrom
                 }
             }
         } else if (unitTemperatureFrom == "Re") {
@@ -270,7 +288,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     temperatureTo = temperatureFrom.times(2.25).plus(491.67)
                 }
                 else -> {
-                    // nothing to do in here
+                    temperatureTo = temperatureFrom
                 }
             }
         }
